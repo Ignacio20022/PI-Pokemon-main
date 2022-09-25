c4eadc7a2d08b26a9b19b1dd6755c16619654162
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-09-24 21:30:01 -0300

    search bar fix

--- client/src/components/Navbar/HomeNavbar.jsx
+++ client/src/components/Navbar/HomeNavbar.jsx
@@ -1,23 +1,33 @@
-import { Link } from 'react-router-dom'
-import SearchBar from '../SearchBar/SearchBar'
-import style from './Navbar.module.css'
+import { Link } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
+import style from "./Navbar.module.css";
 
-export default function Navbar({postsPerPage, setSearch, pag, search}){
-
+export default function Navbar({
+    setPostsPerPage,
+    setSearch,
+    setCurrentPage,
+    search,
+    setMaxLimit,
+    setMinLimit,
+    setPageLimit,
+}) {
     const handleChange = (event) => {
-        event.preventDefault()
-        pag(1)
-        postsPerPage(event.target.value)
-    }
+        event.preventDefault();
+        setCurrentPage(1);
+        setPostsPerPage(event.target.value);
 
-    return(
-        <>  
+    };
+
+    return (
+        <>
             <div className={style.navbar}>
                 <Link to='/pokemons' className={style.link}>
                     <span className={style.home}>Home</span>
                 </Link>
                 <div className={style.selector}>
-                    <label>Pokemons to show <br></br>per page: </label>
+                    <label>
+                        Pokemons to show <br></br>per page:{" "}
+                    </label>
                     <select defaultValue={12} onChange={handleChange}>
                         <option value={1}>1</option>
                         <option value={5}>5</option>
@@ -28,12 +38,18 @@ export default function Navbar({postsPerPage, setSearch, pag, search}){
                     </select>
                 </div>
                 <div className={style.searchbar}>
-                    <SearchBar setSearch={setSearch} pag={pag} search={search}/>
+                    <SearchBar
+                        search={search}
+                        setSearch={setSearch}
+                        setCurrentPage={setCurrentPage}
+                        setMaxLimit={setMaxLimit}
+                        setMinLimit={setMinLimit}
+                    />
                 </div>
                 <Link to='/pokemons/create' className={style.link}>
                     <span className={style.create}>Create</span>
                 </Link>
             </div>
         </>
-    )
+    );
 }
