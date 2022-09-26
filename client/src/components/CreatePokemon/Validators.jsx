acef0cb4f2c5b10e1164926a4946f3700ab9c0c3
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-09-26 16:39:56 -0300

    fix

--- client/src/components/CreatePokemon/Validators.jsx
+++ client/src/components/CreatePokemon/Validators.jsx
@@ -24,23 +24,23 @@ export function validateInputs(input, pokemonsNames) {
     if(input.hp > 255) errors.hp = "The health can't be higher than 255"
 
     if(input.attk.length === 0) errors.attk = "The attack can't be empty (leave 0 if unknown)"
-    if(input.attk < 0) errors.hp = "The attack can't be negative"
+    if(input.attk < 0) errors.attk = "The attack can't be negative"
     if(input.attk > 255) errors.attk = "The attack can't be higher than 255"
 
     if(input.def.length === 0) errors.def = "The defense can't be empty (leave 0 if unknown)"
-    if(input.def < 0) errors.hp = "The defense can't be negative"
+    if(input.def < 0) errors.def = "The defense can't be negative"
     if(input.def > 255) errors.def = "The defense can't be higher than 255"
     
     if(input.speed.length === 0) errors.speed = "The speed can't be empty (leave 0 if unknown)"
-    if(input.speed < 0) errors.hp = "The speed can't be negative"
+    if(input.speed < 0) errors.speed = "The speed can't be negative"
     if(input.speed > 255) errors.speed = "The speed can't be higher than 255"
 
     if(input.height.length === 0) errors.height = "The height can't be empty (leave 0 if unknown)"
-    if(input.height < 0) errors.hp = "The height can't be negative"
+    if(input.height < 0) errors.height = "The height can't be negative"
     if(input.height > 255) errors.height = "The height can't be higher than 255"
 
     if(input.weight.length === 0) errors.weight = "The weight can't be empty (leave 0 if unknown)"
-    if(input.weight < 0) errors.hp = "The weight can't be negative"
+    if(input.weight < 0) errors.weight = "The weight can't be negative"
     if(input.weight > 255) errors.weight = "The weight can't be higher than 255"
 
     if(input.types.length === 0) errors.types = "You have to choose at least one type"
