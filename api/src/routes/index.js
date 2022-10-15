8d517df869c462264fb6579e6be7f4de5e859ef6
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-10-15 18:03:46 -0300

    test

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,17 +1,25 @@
 const { Router } = require('express');
 const pokemonRouter = require('./pokemon');
 const typeRouter = require('./type');
+const authRoute = require("./auth");
+
+
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 
 const router = Router();
 
+
+
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
 router.use('/pokemons', pokemonRouter);
 router.use('/types', typeRouter);
+router.use("/auth", authRoute);
+
 
 
 module.exports = router;
