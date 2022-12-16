8388b2bb6335fbbbb084456a594cfee9a701c0da
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-12-16 18:15:00 -0300

    force redeploy

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,7 @@ import dotenv from 'dotenv'
 import axios from 'axios';
 dotenv.config()
 
-axios.defaults.baseURL = "https://pokemon-pi-ignacio.herokuapp.com"
+axios.defaults.baseURL = process.env.REACT_APP_API
 
 ReactDOM.render(
   <React.StrictMode>
